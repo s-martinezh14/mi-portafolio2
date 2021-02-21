ff9c343836bd2bd03977e2cfcae1abfb198f4064
s-martinezh14 <79294834+s-martinezh14@users.noreply.github.com>
2021-02-20 22:39:17 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/*Evento del botón subir*/
+$(document).ready(function () {
+
+    $('.subir').click(function () {
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 3500);
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.subir').slideDown(500);
+
+        } else {
+            $('.subir').slideUp(500);
+        }
+    });
+
+});
+
+/*Fin evento del botón subir*/
+
+/*Evento del menú de navegación*/
+function scrollNav() {
+    $('.menu-item').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 85
+        }, 2500);
+        return false;
+    });
+    $('.scrollTop a').scrollTop();
+}
+scrollNav();
+
+/*Fin evento del menú de navegación*/
+
+/**/
+M.AutoInit();
+
+/**/
